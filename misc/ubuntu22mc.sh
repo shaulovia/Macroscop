ad58b3ae192c5b1e15ed44fa766c1343a5cad2e6
Ilya A. Shaulov <shaulov@bit-map.ru>
2023-03-10 16:25:10 +0500

    Few changes

--- misc/ubuntu22mc.sh
+++ misc/ubuntu22mc.sh
@@ -5,8 +5,8 @@ sudo chown _apt /etc/apt/trusted.gpg.d/macroscop.gpg
 sudo wget -P /etc/apt/sources.list.d/ http://packages.macroscop.com/deb/macroscop.list
 sudo apt-get update
 mkdir install
-wget -P install/ https://ishaulov.ru/macroscop/ubuntu22/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
+wget -P install/ https://ishaulov.ru/macroscop/ubuntu22/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
 wget -P install/ https://ishaulov.ru/macroscop/ubuntu22/openssl_1.1.1f-1ubuntu2.17_amd64.deb
+sudo dpkg -i -P install/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb
+sudo dpkg -i -P install/openssl_1.1.1f-1ubuntu2.17_amd64.deb
 rm -rf install/
-sudo dpkg -i -P install/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-sudo dpkg -i -P openssl_1.1.1f-1ubuntu2.17_amd64.deb
